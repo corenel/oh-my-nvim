892ce47fc6096f945d7734e891349072e59d272d
corenel <xxdsox@gmail.com>
2024-04-19 15:30:19 +0800

    fix(plugins): ensure the gruvbox theme is loaded

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -8,7 +8,7 @@ return {
 			vim.g.hardhacker_darker = 0
 			vim.g.hardhacker_hide_tilde = 1
 			vim.g.hardhacker_keyword_italic = 1
-			vim.cmd("colorscheme hardhacker")
+			-- vim.cmd("colorscheme hardhacker")
 		end,
 	},
 	{
@@ -17,6 +17,16 @@ return {
 		priority = 1000,
 		config = function()
 			-- vim.cmd("colorscheme tokyonight-storm")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		name = "gruvbox",
+		lazy = false,
+		priority = 1000,
+		config = function()
+            vim.o.background = "dark"
+			vim.cmd("colorscheme gruvbox")
 		end,
 	},
 }
