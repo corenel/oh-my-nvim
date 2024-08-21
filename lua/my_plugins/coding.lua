5d80866f7a97c332f91361e03f06a7b3565c60bb
corenel <xxdsox@gmail.com>
2024-08-21 18:15:52 +0800

    feat(my_plugins): add avante.nvim for AI-driven code suggestions

---
+++ lua/my_plugins/coding.lua
@@ -0,0 +1,43 @@
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		build = ":Copilot auth",
+		opts = {
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+			filetypes = {
+				markdown = true,
+				help = true,
+			},
+		},
+	},
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		build = "make",
+		opts = {
+			-- add any opts here
+			provider = "copilot",
+		},
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			{
+				"grapp-dev/nui-components.nvim",
+				dependencies = {
+					"MunifTanjim/nui.nvim",
+				},
+			},
+			--- The below is optional, make sure to setup it properly if you have lazy=true
+			{
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
+}
